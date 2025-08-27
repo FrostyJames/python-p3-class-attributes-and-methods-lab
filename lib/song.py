d669b6f27963dcf66b77e595fb0c65523a900933
james <ivanlavan773@gmail.com>
2025-08-27 09:42:43 +0300

    finished code

--- lib/song.py
+++ lib/song.py
@@ -1,5 +1,4 @@
 class Song:
-    # Class attributes
     count = 0
     genres = []
     artists = []
@@ -11,47 +10,39 @@ class Song:
         self.artist = artist
         self.genre = genre
 
-        self.add_song_to_count()
-        self.add_to_genres()
-        self.add_to_artists()
-        self.add_to_genre_count()
-        self.add_to_artist_count()
+        Song.add_song_to_count()
+        Song.add_to_genres(self.genre)
+        Song.add_to_artists(self.artist)
+        Song.add_to_genre_count(self.genre)
+        Song.add_to_artist_count(self.artist)
 
     def __repr__(self):
         return f"<Song: {self.name} by {self.artist} ({self.genre})>"
+
     @classmethod
     def add_song_to_count(cls):
         cls.count += 1
 
     @classmethod
-    def add_to_genres(cls):
-        if cls.genres is None:
-            cls.genres = []
-        if cls.genre not in cls.genres:
-            cls.genres.append(cls.genre)
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
 
     @classmethod
-    def add_to_artists(cls):
-        if cls.artists is None:
-            cls.artists = []
-        if cls.artist not in cls.artists:
-            cls.artists.append(cls.artist)
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
 
     @classmethod
-    def add_to_genre_count(cls):
-        if cls.genre in cls.genre_count:
-            cls.genre_count[cls.genre] += 1
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
         else:
-            cls.genre_count[cls.genre] = 1
+            cls.genre_count[genre] = 1
 
     @classmethod
-    def add_to_artist_count(cls):
-        if cls.artist in cls.artist_count:
-            cls.artist_count[cls.artist] += 1
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
         else:
-            cls.artist_count[cls.artist] = 1
-
-
-
-
-
+            cls.artist_count[artist] = 1
