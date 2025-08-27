689028a2e3652f58026504719a610f0f380e5127
james <ivanlavan773@gmail.com>
2025-08-27 09:38:43 +0300

    added __repr__

--- lib/song.py
+++ lib/song.py
@@ -10,12 +10,16 @@ class Song:
         self.name = name
         self.artist = artist
         self.genre = genre
-        
+
         self.add_song_to_count()
         self.add_to_genres()
         self.add_to_artists()
         self.add_to_genre_count()
         self.add_to_artist_count()
+
+    def __repr__(self):
+        return f"<Song: {self.name} by {self.artist} ({self.genre})>"
+
 
 
 
