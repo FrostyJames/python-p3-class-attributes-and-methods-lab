db6518ad751948d30f234fb34cd90845f2994512
james <ivanlavan773@gmail.com>
2025-08-27 09:36:12 +0300

    initialization

--- lib/song.py
+++ lib/song.py
@@ -6,3 +6,9 @@ class Song:
     genre_count = {}
     artist_count = {}
 
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+
